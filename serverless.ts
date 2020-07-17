d335558ae668bb987c271b4fc25055237241ea57
Li Weinan <li.weinan@rea-group.com>
2020-07-17 22:22:24 +0800

    add new function

--- serverless.ts
+++ serverless.ts
@@ -1,44 +1,56 @@
-import { Serverless } from 'serverless/aws';
+import { Serverless } from "serverless/aws";
 
 const serverlessConfiguration: Serverless = {
   service: {
-    name: 'snap-link-api',
+    name: "snap-link-api",
     // app and org for use with dashboard.serverless.com
     // app: your-app-name,
     // org: your-org-name,
   },
-  frameworkVersion: '>=1.72.0',
+  frameworkVersion: ">=1.72.0",
   custom: {
     webpack: {
-      webpackConfig: './webpack.config.js',
-      includeModules: true
-    }
+      webpackConfig: "./webpack.config.js",
+      includeModules: true,
+    },
   },
   // Add the serverless-webpack plugin
-  plugins: ['serverless-webpack'],
+  plugins: ["serverless-webpack"],
   provider: {
-    name: 'aws',
-    runtime: 'nodejs12.x',
+    name: "aws",
+    runtime: "nodejs12.x",
+    region: 'ap-southeast-1',
     apiGateway: {
       minimumCompressionSize: 1024,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
     },
   },
   functions: {
     hello: {
-      handler: 'handler.hello',
+      handler: "handler.hello",
       events: [
         {
           http: {
-            method: 'get',
-            path: 'hello',
-          }
-        }
-      ]
-    }
-  }
-}
+            method: "get",
+            path: "hello",
+          },
+        },
+      ],
+    },
+    genTxtMsg: {
+      handler: "handler.genTxtMsg",
+      events: [
+        {
+          http: {
+            method: "post",
+            path: "genTxtMsg",
+          },
+        },
+      ],
+    },
+  },
+};
 
 module.exports = serverlessConfiguration;
